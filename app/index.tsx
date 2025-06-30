d170c32e5df0879c4351ba5bf635eb3db3d2594e
susil.thapa <susil.thapa@cnspartner.com>
2025-06-30 15:17:25 +0000

    initial setup with nativewind

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="font-semibold text-accent text-5xl">Welcome.</Text>
+    </View>
+  );
+}
